c00407faccbcc1b18b539b47532d80adf66fdb5e
fabricio <fabricio.engenheiro@gmail.com>
2021-05-09 12:09:25 -0300

    update

---
+++ Diversos/Input/imput.asm
@@ -0,0 +1,62 @@
+;--------------------------------------
+; Entrada de dados
+; Eng. Fabrício de Lima Ribeiro
+; 09/04/2021
+;
+; nasm -f elf64 input.asm
+; ld -s -o input input.o
+;--------------------------------------
+
+section .data
+	text1 db "What is your name? "
+	len1  equ $ - text1
+	text2 db "Hello, "
+	len2  equ $ - text2
+
+section .bss
+	name resb 16
+
+section .text
+	global _start
+_start:
+
+	call _printText1
+	call _getName
+	call _printText2
+	call _printName
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+_getName:
+	mov rax, 0
+	mov rdi, 0
+	mov rsi, name
+	mov rdx, 16
+	syscall
+	ret
+
+_printText1:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, text1
+	mov rdx, len1
+	syscall
+	ret
+
+_printText2:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, text2
+	mov rdx, len2
+	syscall
+	ret
+
+_printName:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, name
+	mov rdx, 16
+	syscall
+	ret
